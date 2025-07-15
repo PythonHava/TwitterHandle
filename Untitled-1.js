a69314a4ae7d28bc386be95e205388f00ea42771
PythonHava <anzarmustafa2004@gmail.com>
2025-07-15 14:18:14 +0500

    Initial commit

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,4 +1,40 @@
 
  const TwitterAccount= {
-    Name = 
+    Name : "Anzar Mustafa",
+    DetailsAboutUser : "Co-Founder of CodeWithAnzar, a platform dedicated to teaching programming and web development.",
+    TwitterHandle : "@AnzarMustafa",
+    Following : 1500,
+    Followers : 2000,
+    IsFollow : true,
+    ProfileImage : "https://pbs.twimg.com/profile_images/1234567890/AnzarMustafa.jpg",
+    ProfileBanner : "https://pbs.twimg.com/profile_banners/1234567890/1580000000",
+    Location : "Pakistan",
+    Website : "https://codewithanzar.com",
+    Bio : "Passionate about coding and teaching. Follow me for programming tips and tutorials.",
+    JoinedDate : "January 2020",
+    Tweets : [
+        {
+            TweetId: "1234567890123456789",
+            TweetText: "Excited to launch our new course on JavaScript! Check it out at https://codewithanzar.com/javascript-course",
+            TweetDate: "2023-10-01",
+            Retweets: 100,
+            Likes: 500,
+            Replies: 50,
+            IsRetweet: false,
+            MediaUrl: "https://pbs.twimg.com/media/JavaScriptCourse.jpg"
+        },
+        {
+            TweetId: "9876543210987654321",
+            TweetText: "Just finished a live coding session on React! Watch the replay here: https://codewithanzar.com/react-live",
+            TweetDate: "2023-09-25",
+            Retweets: 200,
+            Likes: 800,
+            Replies: 100,
+            IsRetweet: false,
+            MediaUrl: "https://pbs.twimg.com/media/ReactLiveSession.jpg"
+        }
+    ]
 }
+
+
+console.log(TwitterAccount);
